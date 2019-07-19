1230988f96e829fb130a5ba9748b92502b41f191
Armando <armando.marti.carretero@gmail.com>
2019-07-20 00:50:10 +0200

    computed, methods y watchers

--- appsVue/helloWorld/index.js
+++ appsVue/helloWorld/index.js
@@ -72,6 +72,60 @@ var app8 = new Vue({
         attValue: 'https://www.google.es',
         attName : 'href',
         urlName: 'google'
+    },
+    methods: {
+        doSomething: function() {    
+            urlName = 'other google'
+        }    
+    }
+})
+
+var example = new Vue({
+    el: '#example',
+    data: {
+        message: 'Hola que tal?'
+    },
+    computed: {
+        reversedMessage: function() {
+            return this.message.split('').reverse().join('')
+        },
+        now: function() {
+            return Date.now()
+        }
+    },   
+})
+
+var watchExampleVM = new Vue({
+    el: '#watch-example',
+    data: {
+        question: '',
+        answer: 'No puedo darte una respuesta hasta que me preguntes!'
+    },
+    watch: {
+        question: function(newQuestion, oldQuestion) {
+            this.answer = 'Esperando a que pares de escribir...'
+            this.debouncedGetAnswer()
+        }
+    },
+    created: function() {
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+    },
+    methods: {
+        getAnswer: function() {
+            if (this.question.indexOf('?') === -1) {
+                this.answer = 'Las preguntas usualmente contienen un signo de interrogacion. ;-)'
+                return
+            }
+            this.answer = 'Pensando...'
+            var vm = this
+            axios.get('https://yesno.wtf/api')
+                .then(function (response) {
+                    vm.answer = _.capitalize(response.data.answer)
+                })
+                .catch(function (error){
+                    vm.answer = 'Error! no puedo acceder a la API. ' + error       
+                })
+        }
     }
 })
 
